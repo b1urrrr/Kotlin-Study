d39248dd75a87df8af5ddc541d18f20c573c9229
b1urrrr <b1urrr@naver.com>
2023-02-09 19:42:25 +0900

    [FEAT] 상품 카테고리 메뉴의 사용자 입력값이 없는 경우 재입력 메시지 출력 처리

--- 안드로이드 앱개발 부트캠프/shoppi-kotlin-console-app/src/main/kotlin/Main.kt
+++ 안드로이드 앱개발 부트캠프/shoppi-kotlin-console-app/src/main/kotlin/Main.kt
@@ -8,4 +8,22 @@ fun main(args: Array<String>) {
         원하시는 카테고리를 입력해주세요.
         ***=============================================***
     """.trimIndent())
+
+    val categories = arrayOf("패션", "전자기기", "반려동물용품")
+    for (category in categories) {
+        println(category)
+    }
+    println("=> 장바구니로 이동하시려면 #을 입력해주세요.")
+
+    var selectedCategory = readLine()
+    while(selectedCategory.isNullOrBlank()) {
+        println("값을 입력해주세요.")
+        selectedCategory = readLine()
+    }
+    if (selectedCategory == "#") {
+        // TODO 1. 장바구니로 이동
+    } else {
+        // TODO 2. 카테고리 상품 목록 보여주기
+        // TODO 3. 카테고리 목록에 없는 값을 입력하는 경우
+    }
 }
