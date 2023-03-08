77f9eaf53a5a93a2c3641bf209404316ed3e3aa5
b1urrrr <b1urrr@naver.com>
2023-03-09 00:06:52 +0900

    [FEAT] Bottom Navigation UI 구현

--- shoppi-android/app/src/main/java/com/shoppi/app/MainActivity.kt
+++ shoppi-android/app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -2,11 +2,15 @@ package com.shoppi.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_product_detail)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null
     }
 }
